d3d7ec4144f14ca8aeeb610daab24a3e09b156b0
Nakunanami <roxy.sluyts@gmail.com>
2024-11-29 10:04:51 +0100

    FlightApp update uploading

--- FlightApp_START/FlightApp/FlightApp/Form1.cs
+++ FlightApp_START/FlightApp/FlightApp/Form1.cs
@@ -36,16 +36,82 @@ namespace FlightApp
             Microsoft.Office.Interop.Excel.Workbook xlWerkmap;
             Microsoft.Office.Interop.Excel.Worksheet xlWerkblad;
 
-            int rij = 9;
-            string departure_id, departure, arrivel_id, arrivel_city, max_capacity, actual_capacity, date_of_flight,average_cost, type_of_flight;
 
-            // Openen van de werkmap
-            xlWerkmap = xlToep.Workbooks.Open(Application.StartupPath + @"\FlightApp.xlsx");
-            //Open werkblad
-            xlWerkblad = xlWerkmap.ActiveSheet;
+            // Instellen van het dialoogvenster Openen
+            OpenFileDialog dlgOpen = new OpenFileDialog();
 
-            // Keuzelijst leegmaken
-            listBoxFlights.Items.Clear();
+            //Eigenschappen instellen
+            dlgOpen.Title = "Openen";
+            dlgOpen.FileName = "";
+            dlgOpen.DefaultExt = ".xlsx";
+            dlgOpen.InitialDirectory = Application.StartupPath;
+            dlgOpen.Filter = "Excel (.xlsx) |*.xlsx|Alle bestanden (*.*)|*.*";
+
+            //Dialoogvenster tonen en de keuze opvangen
+            DialogResult resultaat = dlgOpen.ShowDialog();
+
+            //Kijken of de gebruiker "openen" (ok) geklikt heeft
+            if (resultaat == DialogResult.OK)
+            {
+                // Open de werkmap
+                xlWerkmap = xlToep.Workbooks.Open(dlgOpen.FileName);
+                xlWerkblad = xlWerkmap.Sheets[1]; 
+
+                int rij = 2;
+
+                _flightsList.Clear(); 
+
+                // Doorloop het werkblad
+                while (xlWerkblad.Cells[rij, 1].Value != null)
+                {
+                    // Haal de waarden uit de cellen
+                    string date_of_flight = xlWerkblad.Cells[rij, 7].Value.ToString(); // 'yyyy/mm/dd'
+                    string departure = xlWerkblad.Cells[rij, 2].Value.ToString(); 
+                    string arrivel_city = xlWerkblad.Cells[rij, 4].Value.ToString(); 
+                    string actual_capacity = xlWerkblad.Cells[rij, 6].Value.ToString();
+                    string max_capacity = xlWerkblad.Cells[rij, 7].Value.ToString();
+                    string type_of_flight = xlWerkblad.Cells[rij, 9].Value.ToString();
+
+                    
+ 
+                    // Voeg de gegevens toe aan de lijst
+                    _flightsList.Add(new string[] {date_of_flight, departure, arrivel_city, actual_capacity, max_capacity, type_of_flight });
+
+                    rij++; 
+                }
+
+                // Clone the list
+                _filteredFlightsList = _flightsList.Select(flight => (string[])flight.Clone()).ToList();
+
+                // Show the list in the ListBox
+                ShowInList(_filteredFlightsList);
+
+                // Excel afsluiten
+                xlToep.Quit();
+
+            }
+        }
+
+        private void ShowInList(List<string[]> flightsList)
+        {
+            // ListBox leegmaken
+            //listBoxFlights.Items.Clear(); 
+
+            foreach (var flight in _filteredFlightsList)
+            {
+                // Declaratie 
+                string temp = "";
+
+                //datum, departure, arrival, atual and max capacity, type
+
+                // Tekst in label opbouwen met padding
+                temp = (flight[0]).PadRight(5);
+                temp += flight[1].PadRight(15) + " =>" + flight[2].PadRight(20);
+                temp += (flight[3] + "/" + flight[4]).PadRight(10);
+                temp += (flight[5]).PadRight(10);
+
+                listBoxFlights.Items.Add(temp);
+            }
         }
 
         private void afsluitenToolStripMenuItem_Click(object sender, EventArgs e)
