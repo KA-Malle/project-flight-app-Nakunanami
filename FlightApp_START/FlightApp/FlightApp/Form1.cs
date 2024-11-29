98c2c084c0f800ac1390abefe2b9eee8c2819432
Nakunanami <roxy.sluyts@gmail.com>
2024-11-29 09:59:45 +0100

    FlightApp update uploading

--- FlightApp_START/FlightApp/FlightApp/Form1.cs
+++ FlightApp_START/FlightApp/FlightApp/Form1.cs
@@ -14,19 +14,44 @@ namespace FlightApp
 {
     public partial class FormFlightApp : Form
     {
+        /*
+         * Naam: Roxy Sluyts
+         * Klas: 6ADB
+         */
         public FormFlightApp()
         {
             InitializeComponent();
         }
 
-        /*
-         * Naam:
-         * Klas:
-         */
+        // Declaratie
+        List<string[]> _flightsList = new List<string[]>();
+        List<string[]> _filteredFlightsList = new List<string[]>();
 
 
+        private void openToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            // Vergeet niet via "add refrence" het Microsoft Excel toe te voegen
+            // Declaratie
+            Microsoft.Office.Interop.Excel.Application xlToep = new Microsoft.Office.Interop.Excel.Application();
+            Microsoft.Office.Interop.Excel.Workbook xlWerkmap;
+            Microsoft.Office.Interop.Excel.Worksheet xlWerkblad;
 
+            int rij = 9;
+            string departure_id, departure, arrivel_id, arrivel_city, max_capacity, actual_capacity, date_of_flight,average_cost, type_of_flight;
 
+            // Openen van de werkmap
+            xlWerkmap = xlToep.Workbooks.Open(Application.StartupPath + @"\FlightApp.xlsx");
+            //Open werkblad
+            xlWerkblad = xlWerkmap.ActiveSheet;
 
+            // Keuzelijst leegmaken
+            listBoxFlights.Items.Clear();
+        }
+
+        private void afsluitenToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+
+        }
     }
 }
